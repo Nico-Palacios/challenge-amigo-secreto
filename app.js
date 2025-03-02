283b255d72885d49338a23a85d189a426e70a139
Nicolás Palacios <niconebu12@gmail.com>
2025-03-02 16:59:15 -0300

    Agrgando funcionalidad al JavaScript

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
 
 let listaDeAmigos = [];
 
+function agregarAmigo(){
+   let amigo = document.getElementById("amigo").value;
+   amigo == '' ? alert('Por favor ingrese un nombre valido') : listaDeAmigos.push(amigo);
+   limpiarCaja();
+   console.log(listaDeAmigos);
+}
 
+function limpiarCaja(){
+    let limpia = document.querySelector('#amigo');
+    limpia.value = '';
+}
 
 
